2efb022c06830114dc74b6cc7bfd8ae4f210edff
KarLe15 <leffadkarim97@live.fr>
2020-01-30 15:45:11 +0100

    adding json parser 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,18 @@
 import {Component, OnInit} from '@angular/core';
+import {EvalService} from './services/eval.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [EvalService]
 })
 export class AppComponent implements OnInit {
   title = 'evalStep';
 
-  myStyle: object = {};
-  myParams: object = {};
-  width = 100;
-  height = 100;
-
+  constructor(private evalStep: EvalService) {
+  }
   ngOnInit() {
-    this.myStyle = {
-      position: 'fixed',
-      width: '100%',
-      height: '100%',
-      'z-index': -1,
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-    };
-
-    this.myParams = {
-      particles: {
-        number: {
-          value: 200,
-        },
-        size: {
-          value: 7
-        },
-        color: {
-          value: '#392df5'
-        },
-        shape: {
-          polygon: {
-            nb_sides: 9
-          },
-          type: 'circle',
-        },
-        line_linked: {
-          distance: 200
-        }
-      }
-    };
+    console.log(this.evalStep.getDataStructure());
   }
 }
--- src/app/services/eval.service.ts
+++ src/app/services/eval.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import IEvaluation from '../structures/IEvaluation';
 import IStep from '../structures/IStep';
 import {IEnvironment} from '../structures/IEnvironement';
-import IExpression from "../structures/IExpression";
+import IExpression from '../structures/IExpression';
+import * as jsonSteps from './fact_tr.json';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,43 @@ import IExpression from "../structures/IExpression";
 export class EvalService {
 
   constructor() { }
+  private getSubEnvironementsfromJson(expr: any): IExpression {
+    const toString: string = expr.expr.expr;
+    let environements = null;
+    if (expr.expr.envs) {
+      const envs = expr.expr.envs;
+      const keyEnvs = Object.keys(envs);
+      environements = [];
+      // tslint:disable-next-line:prefer-for-of
+      for (let i = 0; i < keyEnvs.length; i++) {
+        const env = envs[keyEnvs[i]];
+        const keyEnv = Object.keys(env);
+        const funEnvs = [];
+        // tslint:disable-next-line:prefer-for-of
+        for (let j = 0; j < keyEnv.length; j++) {
+          const fun = env[keyEnv[j]];
+          let corec = null;
+          if (fun.corec.length !== 0) {
+            corec = fun.corec;
+          }
+          const funEnv = this.getSubEnvironementsfromJson(fun);
+          funEnvs.push({
+            corec,
+            name: keyEnv[j],
+            funEnv
+          });
+        }
+        environements.push({
+          id: i,
+          items: funEnvs
+        });
+      }
+    }
+    return {
+      toString,
+      environements
+    };
+  }
   private getEnvironementsFromJSONStep(step: any): Array<IEnvironment> | null {
     if (step.current_expression.envs) {
       const context = step.current_expression.envs;
@@ -17,17 +55,18 @@ export class EvalService {
       const res: Array<IEnvironment> = [];
       // tslint:disable-next-line:prefer-for-of
       for (let i = 0; i < keysIndex.length; i++) {
-        const env = context[i];
+        const env = context[keysIndex[i]];
         const keysEnv = Object.keys(env);
         const itemsEnv = [];
         // tslint:disable-next-line:prefer-for-of
         for (let j = 0; j < keysEnv.length ; j++) {
           const varEnv = env[keysEnv[j]];
           let corec = null;
-          if (env.corec.length !== 0) {
+          if (varEnv.corec.length !== 0) {
             corec = varEnv.corec;
           }
-          const expr = this.getExpressionFromJSONstep(varEnv);
+          // console.log('getEnvironementsFromJSON', varEnv);
+          const expr = this.getSubEnvironementsfromJson(varEnv);
           itemsEnv.push({
             corec,
             name: keysEnv[i],
@@ -45,16 +84,23 @@ export class EvalService {
   }
 
   private getExpressionFromJSONstep(step: any): IExpression {
+    // console.log('getExpressionFromJSON', step);
     const environements = this.getEnvironementsFromJSONStep(step);
     return {
-      toString: step.expr,
+      toString: step.current_expression.expr,
       environements
     };
   }
 
-  private getStepFromJSONStep(step: any, previous: IStep, next: IStep): IStep {
+  private getStepFromJSONStep(steps: any, index: number, previous: IStep): IStep {
+    const step: any = steps[index];
+    // console.log('getStepFromJSON', step);
     const currentExpression = this.getExpressionFromJSONstep(step);
-    return {
+    let next = null;
+    if (index !== steps.length - 1) {
+      next = this.getStepFromJSONStep(steps, (index + 1), undefined );
+    }
+    const res =  {
       currentExpression,
       previous,
       nexts: [{
@@ -62,19 +108,46 @@ export class EvalService {
         step: next
       }]
     };
+    if (next !== null) {
+      next.previous = res;
+    }
+    return res;
   }
   private parseJSONToIEvaluation(content: string, firstExpression: string): IEvaluation {
     const json: Array<any> = JSON.parse(content);
-    // tslint:disable-next-line:prefer-for-of
-    for (let i = 1; i < json.length; i++) {
-      const stepJSON = json[i];
-
-    }
-
-
+    const firstStep = this.getStepFromJSONStep(json, 0, null);
     return {
       firstExpression,
       firstStep
     };
+  }
+  // temporary function
+  private jsonToIEvaluation(content, firstExpression: string): IEvaluation {
+    // console.log('jsonToIevaluation', content.default);
+    const firstStep = this.getStepFromJSONStep(content.default, 0, null);
+    return {
+      firstExpression,
+      firstStep
+    };
+  }
+
+  /**
+   * Public function available for this service
+   */
+  public getDataStructure(): IEvaluation {
+    // const stringOfJson = JSON.stringify(jsonSteps);
+    // console.log(JSON.parse(stringOfJson).default);
+    // return this.parseJSONToIEvaluation(stringOfJson, '@mock : first expression');
+     return this.jsonToIEvaluation(jsonSteps, '@mock : first expression');
+  }
+  public getNextStep(current: IStep): IStep | null {
+    let next: IStep | null = null;
+    if (current.nexts !== null) {
+      next = current.nexts[0].step;
+    }
+    return next;
+  }
+  public getPreviousStep(current: IStep): IStep | null {
+    return current.previous;
   }
 }
