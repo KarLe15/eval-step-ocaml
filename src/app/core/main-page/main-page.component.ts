4b361622a41b8d0d7e1afdb45cdf26942f43ff40
leffad karim <leffadkarim97@live.fr>
2020-04-07 19:53:38 +0200

    Semaine 7 | PSTL | Merging with strategy maker

--- src/app/core/main-page/main-page.component.ts
+++ src/app/core/main-page/main-page.component.ts
@@ -1,12 +1,13 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {EvalService} from '../../services/eval.service';
 import IEvaluation from '../../structures/IEvaluation';
-import {Subject} from 'rxjs';
+import {ReplaySubject, Subject} from 'rxjs';
 import IOption from '../../structures/IOption';
 import {OptionsService} from '../../services/options.service';
 import {GetAssetsFilesService} from '../../services/get-assets-files.service';
 import {NavigationExtras, Router} from '@angular/router';
 import {FileElement} from '../../file-manager/model/element';
+import {Strategy, StrategyFactory} from '../../structures/Strategy';
 
 
 
@@ -22,12 +23,14 @@ export class MainPageComponent implements OnInit {
   private defMain: string;
   evaluate = false;
   private eventSubject: Subject<IEvaluation> = new Subject<IEvaluation>();
+  private strategiesSubject = new ReplaySubject<Array<Strategy>>();
   private expressions: IEvaluation;
   private filtredExpressions: IEvaluation;
 
   private options: Map<string, IOption>;
   private listeOptionsAffichage = ['liste', 'arbre'];
   optionChoisi = this.listeOptionsAffichage[0];
+  private strategyChoosen: Strategy;
 
   constructor(
     private evaluator: EvalService,
@@ -45,13 +48,16 @@ export class MainPageComponent implements OnInit {
   }
   ngOnInit() {
     this.options = this.OptService.getOptions();
+    this.getAllStrategiesDefined();
+    this.strategiesSubject.subscribe(strategies => {
+      this.strategyChoosen = strategies[0];
+    });
   }
 
   onClick_evaluate() {
     const expression = this.defFun + '\n\nlet _ = ' + this.defMain;
     this.expressions = this.evaluator.getDataStructure(expression, this.defMain);
-    const options = [];
-    this.filtredExpressions = this.evaluator.getEvaluationsWithFilter(this.expressions, options);
+    this.filtredExpressions = this.evaluator.getEvaluationsWithFilter(this.expressions, this.strategyChoosen);
     this.emitEventToChild(this.filtredExpressions);
     this.evaluate = true;
   }
@@ -75,5 +81,26 @@ export class MainPageComponent implements OnInit {
   /* ---------- Changment de l'option d'affichage ------------------*/
   onChoisi(item) {
     this.optionChoisi = item;
+  }
+
+  private getAllStrategiesDefined() {
+    this.filesService.getFile('assets/strategies/contents.json').then(result => {
+      const strategies: Array<Strategy> = [];
+      const content: Array<{name: string, path: string}> = JSON.parse(result);
+      for (const file of content) {
+        this.filesService.getFile('assets/strategies/' + file.path).then(contentF => {
+          const jsonContent = JSON.parse(contentF);
+          const strategy: Strategy = StrategyFactory(jsonContent);
+          strategies.push(strategy);
+          if (strategies.length === content.length) {
+            this.strategiesSubject.next(strategies);
+          }
+        }).catch(err => {
+          console.error('error while loading file', file.path, err);
+        });
+      }
+    }).catch(err => {
+      console.error('error while loading contents file strategies', err);
+    });
   }
 }
